646f364aedf8fe14a0609bf58d0d3250350c2599
Rani Derradj <raniderradj2@gmail.com>
2023-03-27 12:36:03 +0200

    relier la bdd

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -11,7 +11,7 @@ class HomeController extends AbstractController
     #[Route('/home', name: 'app_home')]
     public function index(): Response
     {
-        return $this->render('home/base.html.twig', [
+        return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
         ]);
     }
