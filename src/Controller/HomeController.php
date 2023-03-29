0b2f617c7ada1bdc64038337af0019a4d62a9dea
Rani Derradj <raniderradj2@gmail.com>
2023-03-29 23:23:46 +0200

    pas grand chose de nouveau a  part un script sur index home

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -9,13 +9,16 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\AbstractType;
+use Doctrine\ORM\EntityManagerInterface;
+
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
     public function index(OffersRepository $offersRepository, PaginatorInterface $paginator, Request $request): Response
     {
-        
+  
         $offerId = $request->query->get('offerId');
 
         if ($offerId !== null) {
