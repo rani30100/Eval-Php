a172491607735fb3f5044403d77d11cd3189952e
Rani Derradj <raniderradj2@gmail.com>
2023-03-31 09:21:35 +0200

    pas encore recuperer le tri

--- src/Controller/JobOfferController.php
+++ src/Controller/JobOfferController.php
@@ -3,14 +3,12 @@
 namespace App\Controller;
 
 use App\Entity\Offers;
-use App\Entity\OffersApplication;
 use App\Form\OffersType;
 use App\Form\OffersApplicationType;
 use App\Repository\OffersRepository;
 use App\Repository\JobApplicationRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use App\Repository\OffersApplicationRepository;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
@@ -18,37 +16,24 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class JobOfferController extends AbstractController
 {
     #[Route('/offer/{id}', name: 'app_job_offer')]
-    public function index(Request $request,OffersApplicationRepository $offersApplicationRepository,OffersRepository $offersRepository): Response
-    {       
-            $offers = new OffersType();
-            $form1 = $this->createForm(OffersType::class,$offers, [
-                'data_class' => null,
-            ]);
-            $form1->handleRequest($request);
-            
-            if ($form1->isSubmitted() && $form1->isValid()) {
-                $offersRepository->save($offers, true);
-                return $this->redirectToRoute('app_offer_index', [], Response::HTTP_SEE_OTHER);
-            }  
-
-            $offerApplication = new OffersApplication();
-            $form = $this->createForm(OffersApplicationType::class,$offerApplication);
-            $form->handleRequest($request);
-
-            if ($form->isSubmitted() && $form->isValid()) {
-                $offersApplicationRepository->save($offerApplication, true);
-                return $this->redirectToRoute('app_offer_index', [], Response::HTTP_SEE_OTHER);
-            }    
-
+    public function index(Request $request,OffersRepository $offerRepository): Response
+    {
+        $offer = new Offers();
             //cree une route pour la redirection
             $path_home = $this->generateUrl('app_home');
-        
+            $form = $this->createForm(OffersType::class,$offer);
+            $form->handleRequest($request);
+    
+            if ($form->isSubmitted() && $form->isValid()) {
+                $offerRepository->save($offer, true);
+                return $this->redirectToRoute('app_offer_index', [], Response::HTTP_SEE_OTHER);
+            }    
+    
             return $this->render('offer/index.html.twig', [
                 //j'initie les variable crée
-                'offerApplication' => $offerApplication,
+                'offer' => $offer,
                 'path_home' => $path_home,
-                'form' => $form->createView(),
-                'form1' => $form1
+                'form' => $form->createView()
             ]);
         }
     }
